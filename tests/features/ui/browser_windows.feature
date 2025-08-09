fc3bb4b43791b6811d45ef85990b2a999189d8f9
Mario Victor <mario.rs1996@gmail.com>
2025-08-09 10:47:45 -0300

    feat(test): add header elements to Home Page

--- tests/features/ui/browser_windows.feature
+++ tests/features/ui/browser_windows.feature
@@ -1,4 +1,6 @@
+@only
 Feature: Browser window
 
     Scenario: New Tab
         Given I go to homepage "https://demoqa.com"
+        When I click on Elements Header
--- tests/pages/HomePage.ts
+++ tests/pages/HomePage.ts
@@ -3,9 +3,21 @@ import { Fixture, Given, When, Then } from 'playwright-bdd/decorators';
 
 export @Fixture('homePage') class HomePage{
     readonly page: Page
+    readonly elementsHeader: Locator
+    readonly formsHeader: Locator
+    readonly alertFramesWindowsHeader: Locator
+    readonly widgetsHeader: Locator
+    readonly interactionsHeader: Locator
+    readonly bookStoreApplicationHeader: Locator
 
     constructor(page: Page){
-        this.page = page
+        this.page = page       
+        this.elementsHeader = this.page.locator("h5",{hasText: "Elements"})
+        this.formsHeader = this.page.locator("h5",{hasText: "Forms"})
+        this.alertFramesWindowsHeader = this.page.locator("h5",{hasText: "Alerts, Frame & Windows"})
+        this.widgetsHeader = this.page.locator("h5",{hasText: "Widgets"})
+        this.interactionsHeader = this.page.locator("h5",{hasText: "Interactions"})
+        this.bookStoreApplicationHeader = this.page.locator("h5",{hasText: "Book Store Application"})
     }
 
     @Given('I go to homepage {string}')
@@ -22,8 +34,33 @@ export @Fixture('homePage') class HomePage{
         await this.page.goto(url);
     }
 
+    @When('I click on Elements Header')
+    async clickOnElementsHeader(){
+        await this.elementsHeader.click()
+    }
+
+    @When('I click on Forms Header')
+    async clickOnFormsHeader(){
+        await this.formsHeader.click()
+    }
+
     @When('I click on Alert Frame Windows')
     async clickOnAlertFrameWindows(){
-        await this.page.locator("span").click()
+        await this.alertFramesWindowsHeader.click()
+    }
+
+    @When('I click on Widgets Header')
+    async clickOnWidgetsHeader(){
+        await this.widgetsHeader.click()
+    }
+
+    @When('I click on Interactions Header')
+    async clickOnInteractionsHeader(){
+        await this.interactionsHeader.click()
+    }
+
+    @When('I click on Elements')
+    async clickOnBookStoreApplicationHeader(){
+        await this.bookStoreApplicationHeader.click()
     }
 }    
