51674e7f6b70b1d46c5c2e198a6270175e2d0ce8
Mario Victor <mario.rs1996@gmail.com>
2025-08-29 22:11:15 -0300

    fix: remove old files

--- fixtures.ts
+++ fixtures.ts
@@ -1,13 +1,10 @@
 import { test as base } from 'playwright-bdd';
-import { ExamplePage } from './tests/pages/ExamplePage';
 import { HomePage } from './tests/pages/HomePage';
 
 type Fixtures = {
-    examplePage: ExamplePage,
     homePage: HomePage,
 }
 
 export const test = base.extend<Fixtures>({
-  examplePage: ({ page }, use) => use(new ExamplePage(page)),
   homePage: ({page}, use) => use(new HomePage(page)),
 });
